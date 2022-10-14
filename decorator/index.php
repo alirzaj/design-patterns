5a3db5ed4bbf018a226cfb34b73da5fd832991a1
AL!R3Z4 <alireza.jazayerei@gmail.com>
2022-10-14 12:29:35 +0330

    decorator

--- decorator/index.php
+++ decorator/index.php
@@ -1,5 +1,12 @@
 <?php
 
+use decorator\CachedThirdPartyApiSdk;
+use decorator\ThirdPartyApiSdk;
+
 include __DIR__ . "/../vendor/autoload.php";
 
 $thirdPartyApiSdk = new ThirdPartyApiSdk();
+$cachedThirdPartyApiSdk = new CachedThirdPartyApiSdk($thirdPartyApiSdk);
+
+//echo $thirdPartyApiSdk->getAll();
+echo $cachedThirdPartyApiSdk->getAll();
